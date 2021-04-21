fd15b335377cc47d3edeec06c2e221eb48d350eb
Mapl92 <maximilian.pleickhardt@stud.uni-bamberg.de>
2021-04-21 21:33:31 +0200

    allowed hosts

--- hebesatz/settings.py
+++ hebesatz/settings.py
@@ -62,11 +62,9 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "https://example.com",
-    "https://sub.example.com",
+
     "http://localhost:8080",
-    "http://localhost:8080/",
-    "http://127.0.0.1:9000"
+    "http://127.0.0.1:8080"
 ]
 
 CORS_ALLOW_METHODS = [
