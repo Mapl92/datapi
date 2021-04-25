d6f6a85bc9fc15613da8968548fe63ab831e18c0
Mapl92 <maximilian.pleickhardt@stud.uni-bamberg.de>
2021-04-25 21:11:23 +0200

    corsheaders

--- hebesatz/settings.py
+++ hebesatz/settings.py
@@ -62,12 +62,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-
-    "http://muellerblum.taxapi.io",
-    "http://localhost:8080",
-    "http://127.0.0.1:8080"
-]
+CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ALLOW_METHODS = [
     'DELETE',
